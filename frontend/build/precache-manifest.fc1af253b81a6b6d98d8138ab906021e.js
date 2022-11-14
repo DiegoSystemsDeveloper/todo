189db4c973fb8c9807c592f17cd59c899fe6f0ff
Diego perez <hhola3628@gmail.com>
2022-11-13 20:47:25 -0500

    Build

---
+++ frontend/build/precache-manifest.fc1af253b81a6b6d98d8138ab906021e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2659e104d5347f06524d",
+    "url": "/static/css/main.4001a154.chunk.css"
+  },
+  {
+    "revision": "2659e104d5347f06524d",
+    "url": "/static/js/main.2659e104.chunk.js"
+  },
+  {
+    "revision": "ae4700ead3b6e1632dda",
+    "url": "/static/js/1.ae4700ea.chunk.js"
+  },
+  {
+    "revision": "84fc2415af7851649852",
+    "url": "/static/js/2.84fc2415.chunk.js"
+  },
+  {
+    "revision": "95228a3a9e71a7d2dd9f",
+    "url": "/static/js/runtime~main.95228a3a.js"
+  },
+  {
+    "revision": "1d96aba4105f57e1edaf1967ee2b7561",
+    "url": "/index.html"
+  }
+];
